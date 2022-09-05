27887cf275b3c53a5b384e6c7787e92cfbb0398d
Narongsak Jaroenpanyasak <89260519+Justzs@users.noreply.github.com>
2022-09-05 17:02:27 +0700

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,22 +1,18 @@
 public class InsertionSort {
 
-    public static void sort(int arr[])
-    {
-        int n = arr.length;
-        for (int i = 1; i < n; ++i) {
-            int key = arr[i];
-            int j = i - 1;
-
-            /* Move elements of arr[0..i-1], that are
-               greater than key, to one position ahead
-               of their current position */
-            while (j >= 0 && arr[j] > key) {
+    public static void sort(int[] arr) {
+        int size = arr.length;
+        for (int step = 1; step < size; step++) {
+            int key = arr[step];
+            int j = step - 1;
+            while (j >= 0 && key > arr[j]) {
                 arr[j + 1] = arr[j];
-                j = j - 1;
+                --j;
             }
             arr[j + 1] = key;
         }
     }
+
 
     // Driver method
     public static void main(String[] args) {
@@ -24,19 +20,18 @@ public class InsertionSort {
         int repeat = 10;
         int maxList = 20000;
         int range = 2000;
-        for(int n=0; n<=maxList; n+= range){
+        for (int n = 2000; n <= maxList; n += range) {
             int[] d = new int[n];
             long sum = 0;
-            for (int k=0; k<repeat; k++){
-                for (int i=n-1; i>0; i--){
+            for (int k = 0; k < repeat; k++) {
+                for (int i = n - 1; i > 0; i--) {
                     d[i] = i;
                 }
                 long start = System.currentTimeMillis();
                 sort(d);
-                sum = sum + System.currentTimeMillis() - start;
+                sum += (System.currentTimeMillis() - start);
             }
-            System.out.printf("%d \t %d \t %f \n", n, sum, (double)sum/repeat);
-
+            System.out.printf("%d \t %d \t %f \n", n, sum, (double) sum / repeat);
         }
     }
 }
