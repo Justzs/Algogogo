da392989161cbf78c37b93b49fe5fc70b970300e
Narongsak Jaroenpanyasak <89260519+Justzs@users.noreply.github.com>
2022-09-06 12:58:48 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,42 @@
+let counter=0;
+function sort(arr)  
+{  
+    let i, key, j;  
+    for (i = 1; i < arr.length; i++) 
+    {  
+        key = arr[i];  
+        j = i - 1;  
+        while (j >= 0 && arr[j] < key) 
+        {  
+            counter+=1;
+            arr[j + 1] = arr[j];  
+            j = j - 1;  
+        }  
+        arr[j + 1] = key;  
+    }  
+}    
+
+function main(){
+    const repeat = 10;
+    const maxList = 20000;
+    let range = 2000;
+    let n, k, i;
+    for (n=2000; n<=maxList; n+= range){
+        let d = []; 
+        let sum = 0;
+        for(k=0; k<repeat; k++){
+            for(i=n-1; i>0; i--){
+                d[i] = i;
+            }
+            // counter = 0;
+            let start = Date.now();
+            sort(d);
+            let end = Date.now();
+            sum += end - start;
+            // sum+=counter;
+        }
+        console.log(`${n}\t${sum}\t${sum/repeat}`);
+    }
+}
+
+main()
